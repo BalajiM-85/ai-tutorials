f715ee5882854be50ecabbd32e3963c902c7998b
BalajiM-85 <balaji.psgmech@gmail.com>
2026-06-21 21:58:04 +0530

    Agentic RAG using RAG Tool

--- rag_search.py
+++ rag_search.py
@@ -97,3 +97,43 @@ rag_chain = (
 query    = "What is the LinkedIn revenue?"
 response = rag_chain.invoke(query)
 print(response)
+
+# ============================================================
+# 🚀 STEP 7 — Create RAG Tool for Agentic RAG
+# ============================================================
+from langchain.agents import create_agent
+from langchain_community.tools import StructuredTool
+from langchain_core.messages import HumanMessage
+from pydantic import BaseModel
+
+class RAGInput(BaseModel):
+    query: str
+
+rag_tool = StructuredTool(
+    name="MS_Earnings_RAG",
+    func=lambda query: rag_chain.invoke(query),
+    description="Useful for answering questions about MicroSoft Earnings",
+    args_schema=RAGInput
+)
+
+tools=[rag_tool]
+
+# ============================================================
+# 🕵️ AGENT
+# ============================================================
+agent_executor = create_agent(model, tools)
+
+# ============================================================
+# 🚀 RUN
+# ============================================================
+query = "What is the news about copilot adoption?"
+
+response = agent_executor.invoke({
+    "messages": [HumanMessage(content=query)]
+})
+
+# ✅ Extract last non-empty response
+for msg in reversed(response["messages"]):
+    if hasattr(msg, "content") and msg.content.strip():
+        print(msg.content)
+        break
